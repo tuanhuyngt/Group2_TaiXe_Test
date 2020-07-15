2a490fe097a77938a9deb85ace09ef1d0b4ef7b2
tuanhuyngt <tuanhuyngt@gmail.com>
2020-07-16 03:25:09 +0700

    50%

---
+++ Group2_TaiXe_Test/DriverAdd.cs
@@ -0,0 +1,90 @@
+﻿using Group2_TaiXe_Test.BaseClass;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Group2_VatTu_Test
+{
+    public class DriverAdd : BaseTest
+    {
+        IWebElement ele;
+        string str;
+        IWebElement txtTen, txtMucLuong, txtNgayBatDau, txtHangGPLX, btnLuu;
+
+        void GetElement()
+        {
+            ele = webDriver.FindElement(By.XPath("/html/body/app-root/ng-component/div/div/div[2]/app-driver/app-driver-management-group2/div/div[1]/div[1]/div/ul/li[1]"));
+            ele.Click();
+            Thread.Sleep(200);
+            txtTen = webDriver.FindElement(By.XPath("/html/body/app-root/ng-component/div/div/div[2]/app-driver-add/app-driver-add-group2/div/div[2]/div/div[1]/form/div[1]/div[1]/div/input"));
+            txtMucLuong = webDriver.FindElement(By.XPath("/html/body/app-root/ng-component/div/div/div[2]/app-driver-add/app-driver-add-group2/div/div[2]/div/div[1]/form/div[2]/div[1]/div/input"));
+            txtNgayBatDau = webDriver.FindElement(By.XPath("/html/body/app-root/ng-component/div/div/div[2]/app-driver-add/app-driver-add-group2/div/div[2]/div/div[1]/form/div[1]/div[2]/div/input"));
+            txtHangGPLX = webDriver.FindElement(By.XPath("/html/body/app-root/ng-component/div/div/div[2]/app-driver-add/app-driver-add-group2/div/div[2]/div/div[1]/form/div[2]/div[2]/div/p-dropdown"));
+            btnLuu = webDriver.FindElement(By.XPath("/html/body/app-root/ng-component/div/div/div[2]/app-driver-add/app-driver-add-group2/div/div[2]/div/div[1]/div/div[2]/div/form/div/div/ul/li[2]"));
+        }
+
+        [Test, Category("RightInputDriverAdd"), Order(1)]
+        public void txtTenCheck()
+        {
+            this.NagativeToManagement();
+            this.GetElement();
+            txtTen.SendKeys("Tuấn Huy");
+            str = txtTen.GetAttribute("value");
+            Assert.AreEqual("Tuấn Huy", str);
+        }
+        [Test, Category("RightInputDriverAdd"), Order(2)]
+        public void txtMucLuongCheck()
+        {
+            txtMucLuong.SendKeys("2oio0000000");
+            str = txtMucLuong.GetAttribute("value");
+            Assert.AreEqual("20000000", str);
+        }
+        [Test, Category("RightInputDriverAdd"), Order(3)]
+        public void txtNgayBatDauCheck()
+        {
+            txtNgayBatDau.SendKeys("02012020");
+            str = txtNgayBatDau.GetAttribute("value");
+            Assert.AreEqual("2020-02-01", str);
+        }
+
+        [Test, Category("RightInputDriverAdd"), Order(4)]
+        public void txtHangGPLXCheck()
+        {
+            txtHangGPLX.Click();
+            Thread.Sleep(300);
+            ele = webDriver.FindElement(By.XPath("/html/body/app-root/ng-component/div/div/div[2]/app-driver-add/app-driver-add-group2/div/div[2]/div/div[1]/form/div[2]/div[2]/div/p-dropdown/div/div[4]/div/ul/li[3]"));
+            ele.Click();
+            str = txtHangGPLX.Text;
+            Assert.AreEqual("B2", str);
+        }
+        [Test, Category("RightInputDriverAdd"), Order(5)]
+        public void modalXacNhanCheck()
+        {
+            btnLuu.Click();
+            ele = webDriver.FindElement(By.XPath("/html/body/app-root/ng-component/div/div/div[2]/app-driver-add/app-driver-add-group2/div/p-dialog/div/div[2]"));
+            str = ele.Text;
+            Assert.AreEqual("Xác nhận thêm tài xế?", str);
+        }
+
+        [Test, Category("RightInputDriverAdd"), Order(7)]
+        public void cancelXacnhan()
+        {
+            ele = webDriver.FindElement(By.XPath("/html/body/app-root/ng-component/div/div/div[2]/app-driver-add/app-driver-add-group2/div/p-dialog/div/div[3]/p-footer/button[2]"));
+            ele.Click();
+        }
+        [Test, Category("RightInputDriverAdd"), Order(8)]
+        public void confirmXacNhanCheck()
+        {
+            btnLuu.Click();
+            ele = webDriver.FindElement(By.XPath("/html/body/app-root/ng-component/div/div/div[2]/app-driver-add/app-driver-add-group2/div/p-dialog/div/div[3]/p-footer/button[1]"));
+            ele.Click();
+            ele = webDriver.FindElement(By.ClassName("toast-message"));
+            str = ele.Text;
+            Assert.AreEqual("Thêm tài xế thành công", str);
+        }
+
+    }
+}
---
+++ Group2_TaiXe_Test/DriverManagement.cs
@@ -0,0 +1,115 @@
+using Group2_TaiXe_Test.BaseClass;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using System.Threading;
+
+namespace Group2_TaiXe_Test
+{
+    public class DriverManagement : BaseTest
+    {
+        IWebElement ele, pLoaiTimKiem, liMa, liTen, liHangBangLai, liLuongToiThieu, liLuongToiDa;
+        string str;
+
+        void GetElement()
+        {
+            pLoaiTimKiem = webDriver.FindElement(By.XPath("/html/body/app-root/ng-component/div/div/div[2]/app-driver/app-driver-management-group2/div/div[2]/div/div[1]/div/p-multiselect"));
+            liMa = webDriver.FindElement(By.XPath("/html/body/app-root/ng-component/div/div/div[2]/app-driver/app-driver-management-group2/div/div[2]/div/div[1]/div/p-multiselect/div/div[4]/div[2]/ul/li[1]"));
+            liTen = webDriver.FindElement(By.XPath("/html/body/app-root/ng-component/div/div/div[2]/app-driver/app-driver-management-group2/div/div[2]/div/div[1]/div/p-multiselect/div/div[4]/div[2]/ul/li[2]"));
+            liHangBangLai = webDriver.FindElement(By.XPath("/html/body/app-root/ng-component/div/div/div[2]/app-driver/app-driver-management-group2/div/div[2]/div/div[1]/div/p-multiselect/div/div[4]/div[2]/ul/li[3]"));
+            liLuongToiThieu = webDriver.FindElement(By.XPath("/html/body/app-root/ng-component/div/div/div[2]/app-driver/app-driver-management-group2/div/div[2]/div/div[1]/div/p-multiselect/div/div[4]/div[2]/ul/li[4]"));
+        }
+
+        [Test, Category("DirectButtonManagement"), Order(4)]
+        public void txtTimKiemMaCheck()
+        {
+            this.GetElement();
+            pLoaiTimKiem.Click();
+            Thread.Sleep(300);
+            liMa.Click();
+            ele = webDriver.FindElement(By.XPath("/html/body/app-root/ng-component/div/div/div[2]/app-driver/app-driver-management-group2/div/div[2]/div/div[1]/form/div/input"));
+            ele.SendKeys("2");
+            ele.SendKeys(Keys.Enter);
+            Thread.Sleep(1000);
+        }
+        [Test, Category("DirectButtonManagement"), Order(5)]
+        public void txtTimKiemTenCheck()
+        {
+            pLoaiTimKiem.Click();
+            Thread.Sleep(300);
+            liMa.Click();
+            Thread.Sleep(300);
+            liTen.Click();
+            ele = webDriver.FindElement(By.XPath("/html/body/app-root/ng-component/div/div/div[2]/app-driver/app-driver-management-group2/div/div[2]/div/div[1]/form/div/input"));
+            ele.SendKeys("u");
+            ele.SendKeys(Keys.Enter);
+            Thread.Sleep(1000);
+        }
+        [Test, Category("DirectButtonManagement"), Order(6)]
+        public void txtHangBangLaiCheck()
+        {
+            pLoaiTimKiem.Click();
+            Thread.Sleep(300);
+            liTen.Click();
+            Thread.Sleep(300);
+            liHangBangLai.Click();
+            ele = webDriver.FindElement(By.XPath("/html/body/app-root/ng-component/div/div/div[2]/app-driver/app-driver-management-group2/div/div[2]/div/div[1]/form/div/input"));
+            ele.SendKeys("B1");
+            ele.SendKeys(Keys.Enter);
+            Thread.Sleep(1000);
+        }
+
+        [Test, Category("DirectButtonManagement"), Order(7)]
+        public void txtLuongToiThieuCheck()
+        {
+            pLoaiTimKiem.Click();
+            Thread.Sleep(300);
+            liHangBangLai.Click();
+            Thread.Sleep(300);
+            liLuongToiThieu.Click();
+            ele = webDriver.FindElement(By.XPath("/html/body/app-root/ng-component/div/div/div[2]/app-driver/app-driver-management-group2/div/div[2]/div/div[1]/form/div/input"));
+            ele.SendKeys("15000000");
+            ele.SendKeys(Keys.Enter);
+            Thread.Sleep(1000);
+        }
+
+
+        [Test, Category("DirectButtonManagement"), Order(1)]
+        public void btnThemMoiCheck()
+        {
+            this.NagativeToManagement();          
+            ele = webDriver.FindElement(By.XPath("/html/body/app-root/ng-component/div/div/div[2]/app-driver/app-driver-management-group2/div/div[1]/div[1]/div/ul/li[1]"));
+            ele.Click();
+            ele = webDriver.FindElement(By.XPath("/html/body/app-root/ng-component/div/div/div[2]/app-driver-add/app-driver-add-group2/div/div[1]/div[2]/div/div/span[1]"));
+            str = ele.Text;
+            Assert.AreEqual("Thêm tài xế", str);
+            webDriver.Navigate().Back();
+            Thread.Sleep(300);
+        }
+        [Test, Category("DirectButtonManagement"), Order(2)]
+        public void btnChinhSua()
+        {          
+            ele = webDriver.FindElement(By.XPath("/html/body/app-root/ng-component/div/div/div[2]/app-driver/app-driver-management-group2/div/div[1]/div[1]/div/ul/li[2]"));
+            ele.Click();
+            ele = webDriver.FindElement(By.XPath("/html/body/app-root/ng-component/div/div/div[2]/app-driver-edit/app-driver-edit-group2/div/div[1]/div[2]/div/div/span[1]"));
+            str = ele.Text;
+            Assert.AreEqual("Chỉnh sửa tài xế", str);
+            webDriver.Navigate().Back();
+            Thread.Sleep(300);
+        }
+        [Test, Category("DirectButtonManagement"), Order(3)]
+        public void btnXoa()
+        {
+            ele = webDriver.FindElement(By.XPath("/html/body/app-root/ng-component/div/div/div[2]/app-driver/app-driver-management-group2/div/div[2]/div/div[2]/div/p-table/div/div/table/tbody/tr[5]/td[1]/label/span"));
+            ele.Click();
+            ele = webDriver.FindElement(By.XPath("/html/body/app-root/ng-component/div/div/div[2]/app-driver/app-driver-management-group2/div/div[1]/div[1]/div/ul/li[3]"));
+            ele.Click();
+            Thread.Sleep(200);
+            ele = webDriver.FindElement(By.ClassName("swal-text"));
+            str = ele.Text;
+            Assert.AreEqual("Xác nhận xóa tài xế", str);
+            ele = webDriver.FindElement(By.ClassName("swal-button--cancel"));
+            ele.Click();
+            Thread.Sleep(500);
+        }
+    }
+}
